8198088b754abb2985dc948a534750f316b2b0d1
RAJA SAHU <117908592+kingstarRaj@users.noreply.github.com>
2023-09-16 19:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function decimalToBinary(num) {
-	let str = "";
-	while(num>0){
-	  str += num%2 ; 
-	  num = num/2 ;
-  }
-	str.split("").reverse().join("");
+	// array to store binary number
+        int[] binaryNum = new int[32];
+ 
+        // counter for binary array
+        int i = 0;
+        while (num > 0) {
+            // storing remainder in binary array
+            binaryNum[i] = num % 2;
+            num = num / 2;
+            i++;
+        }
+ 
+        // printing binary array in reverse order
+        for (int j = i - 1; j >= 0; j--)
+            System.out.print(binaryNum[j]);
   
 }
 
